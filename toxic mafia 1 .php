aa9d4e50c9e42b4ba392dbb9ae7aa8ca33158ef3
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-03-21 23:05:53 +0600

    Add files via upload

---
+++ toxic mafia 1 .php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<title>Hacked by ᵀᵒˣⁱᶜ ★𝐌𝐀𝐅𝐈𝐀★ !</title>
+<link href='http://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="http://smilenet4u.googlecode.com/files/TipingText.js"></script>
+</head>
+<style type="text/css">
+/* Coded by CYB3R M4D */
+@import url(http://fonts.googleapis.com/css?family=Share+Tech);
+body {
+    background:black url('http://4.bp.blogspot.com/-h1QH3MmXd4M/Uc64vsqmPeI/AAAAAAAAALs/Gi-fPnZ5V2Q/s1600/black+(11).jpg') center right no-repeat; 
+    color:#FFFFFF; 
+    text-decoration:none; 
+    font-family:"Share Tech", Courier, monospace; 
+    padding-left:200px; 
+    padding-top:200px; 
+    padding-right:300px; 
+    font-size:16px;
+}
+a {
+    color: #FF0000;
+    text-decoration: none;
+}
+a:hover {
+    color: #fff;
+}
+#example1 {
+    font-size: 42px;
+    font-family: "Share Tech";
+ 
+}
+#example2 {
+    font-size: 31px;
+    font-family: "Share Tech";
+ 
+}
+#example3 {
+    font-size: 26px;
+    font-family: "Share Tech";
+ 
+}
+</style>
+<body><script type='text/javascript' id="__bs_script__">//<![CDATA[
+    document.write("<script async src='/browser-sync/browser-sync-client.2.8.2.js'><\/script>".replace("HOST", location.hostname));
+//]]></script>
+
+<object type="application/x-shockwave-flash" width="0"
+height="0"data="=true">
+<param  name="movie"value="http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/4859931381309671.mp3&song_title=uploadmusic.org&autoplay=true"/>
+</object>
+ 
+<div id="example1">OWN3D BY <br>
+<font color="red">ᵀᵒˣⁱᶜ ★𝐌𝐀𝐅𝐈𝐀★</font>
+</div>
+<br>
+<div id="example2">
+<font color="#FFFFFF">
+Message for Admin :<br>Sorry Admin !<br>
+Your Site Has Been Hacked System Security Is Low , Please Patch Your System.
+<br>This Is Just A Warning If You Still Dont want to patch it<br>
+We Will Keep On Hacking it. <br>
+<br><br></div>
+Greetz To :<br>
+<div id="example3">
+<font color="red">
+Innocents Hack3r - Prappo Prince - Darks Miumaz - No More - k44jlgujj4r - Black Tiger - Rj ANmol - WaqaR Akram - United Bangladeshi Hackers
+ 
+</font>
+</div>
+ 
+<script type="text/javascript">
+new TypingText(document.getElementById("example1"));
+new TypingText(document.getElementById("example3"));
+ 
+ 
+new TypingText(document.getElementById("example2"), 150, function(i){ var ar = new Array("_"); return " " + ar[i.length % ar.length]; });
+ 
+TypingText.runAll();
+</script>
+<br>
+<p>
+<a href="https://www.facebook.com/groups/ᵀᵒˣⁱᶜ ★𝐌𝐀𝐅𝐈𝐀★/"> 
+Contact Me </a> and like my  :<a href="https://www.facebook.com/ᵀᵒˣⁱᶜ ★𝐌𝐀𝐅𝐈𝐀★">fan page</a> 
+<a href="https://www.facebook.com/groups/united.bangladeshi.hacker"> Our Group </a></p>
+</body>
+</html>
